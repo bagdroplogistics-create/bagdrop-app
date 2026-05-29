951809339f407e49b3eb8952ae6b8dc51da08383
bagdroplogistics-create <bagdroplogistics@gmail.com>
2026-05-29 12:05:35 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,10 @@ import "./App.css";
 import React from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { Toaster } from "./components/ui/sonner";
+import { AuthProvider } from "./context/AuthContext";
+import RequireAuth from "./components/RequireAuth";
 import Onboarding from "./pages/Onboarding";
+import Login from "./pages/Login";
 import Home from "./pages/Home";
 import Booking from "./pages/Booking";
 import Track from "./pages/Track";
@@ -14,15 +17,18 @@ function App() {
     <div className="App">
       <Toaster position="top-center" />
       <BrowserRouter>
+    <AuthProvider>
         <Routes>
           <Route path="/" element={<Onboarding />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/book/:serviceId" element={<Booking />} />
-          <Route path="/track" element={<Track />} />
-          <Route path="/history" element={<History />} />
-          <Route path="/profile" element={<Profile />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/home" element={<RequireAuth><Home /></RequireAuth>} />
+          <Route path="/book/:serviceId" element={<RequireAuth><Booking /></RequireAuth>} />
+          <Route path="/track" element={<RequireAuth><Track /></RequireAuth>} />
+          <Route path="/history" element={<RequireAuth><History /></RequireAuth>} />
+          <Route path="/profile" element={<RequireAuth><Profile /></RequireAuth>} />
           <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
+     </AuthProvider>
       </BrowserRouter>
     </div>
   );
